b394196faa96d935e492549f9e69d1a4daaf62b1
andpromobile <andpromobile@gmail.com>
2023-06-18 18:04:15 +0800

    First Commit

---
+++ app/src/main/java/umbjm/ft/inf/PengaduanActivity.kt
@@ -0,0 +1,12 @@
+package umbjm.ft.inf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PengaduanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengaduan)
+    }
+}
